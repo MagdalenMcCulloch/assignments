473cdbf1415918311eb2ae185c0cab5ec91c50e6
root <root@DESKTOP-TOUT0BF.localdomain>
2022-01-25 19:36:22 -0500

    assignment 1 excercise

--- A01/password.c
+++ A01/password.c
@@ -1,3 +1,10 @@
 int main() {
+
+// declaring variables
+int mem; //memory location
+char word[50]; //char array for entered word
+printf("type in a word to create a bad password"); 
+scanf("%s",&word); 
+printf("s",word); 
   return 0;
 }
