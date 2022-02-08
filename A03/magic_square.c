4900c6ca29ead40d3297537904f0773f2b02fa0f
root <root@DESKTOP-TOUT0BF.localdomain>
2022-02-08 13:08:07 -0500

    some changes. NOT DONE

--- A03/magic_square.c
+++ A03/magic_square.c
@@ -2,11 +2,44 @@
 // magic_square.c 
 // CS223 - Spring 2022
 // Identify whether a matrix is a magic square
-// Name:
+// Name: Magdalen McCulloch 
 //
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+int main() { // I think this needs to take the arg of the text file? 
+    // variables 
+    int columnsNum = 0;
+    int rowsNum = 0; 
+
+    int* val; 
+
+    //read in the file 
+    scanf(" %d",&val); 
+    //assign values to the rows and columns 
+    columnsNum = val[0]; 
+    rowsNum = val[1]; 
+    //initialize the matrix
+    int** matrixSquare; 
+    
+    matrixSquare = malloc(columnsNum* sizeof(int)); 
+    for(int i =0; i < rowsNum; i++){
+        matrixSquare[i] = malloc(rowsNum * sizeof(int)); 
+    }
+
+    //define in the matrix 
+    // it will go column by column 
+    for (int i = 0; i < columnsNum; i++){
+        matrixSquare = i; 
+        //it will stop at every spot in each row 
+        for(int i = 0; i <rowsNum; i++){
+
+             
+        }
+
+    }
+
+
+
 }
 
--- A03/sorted_snackbar.c
+++ A03/sorted_snackbar.c
@@ -2,20 +2,21 @@
 // sorted_snackbar.c 
 // CS223 - Spring 2022
 // Ask the user for a list of snacks and store them in alphabetical order
-// Name:
+// Name: Magdalen McCulloch 
 //
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-struct snack {
+struct snack { // this is the node i think 
   char name[32];
   int quantity;
   float cost;
   struct snack* next;
 };
 
-// Insert a new node to a list (implemented as a linked list). 
+// Insert a new node to a list (implemented as a linked list).  
+  // yes this is why its called insert sorted 
 // The new node should store the given properties
 // Param snacks: the first item in the list (NULL if empty)
 // Param name: the snack name (max length is 32 characters)
@@ -24,6 +25,36 @@ struct snack {
 // Returns the first item in the list
 struct snack* insert_sorted(struct snack* snacks, 
   const char* name, int quantity, float cost) {
+
+    //represents the current node 
+    // not sure if the way I used pointers will work
+    struct snack currentSnack = *(struct snack*)malloc(sizeof(struct snack)); 
+    //struct snack head;
+
+    int snacksNum;
+    printf("enter a number of snacks: ");
+    scanf("%d",&snacksNum);
+
+    for(int i =0; i<snacksNum; i++){
+      printf("enter the snack name: ");
+      scanf("%s", currentSnack.name); 
+      
+      printf("enter the cost: ");
+      scanf("%f", &currentSnack.cost); 
+
+      printf("enter the quantity: ");
+      scanf("%d", &currentSnack.quantity);
+    }
+    //represents the current node 
+    struct snack* currentSnack = (struct snack*)malloc(sizeof(struct snack)); 
+    //struct snack head;
+    
+
+   if(head == NULL){
+     printf("Error malloc\n"); 
+     exit(1); 
+   } 
+
 
   // todo
   return NULL;
